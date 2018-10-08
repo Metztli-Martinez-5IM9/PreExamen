969595f6412797e52f0630b68e20fb928755328d
Metztli-Martinez-5IM9 <42838947+Metztli-Martinez-5IM9@users.noreply.github.com>
2018-10-08 08:49:55 -0500

    Add files via upload

---
+++ PreExamen/src/java/mx/edu/ipn/cecyt9/edusite/model/Rol.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.ipn.cecyt9.edusite.model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Alumno
+ */
+
+public class Rol implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private Integer idRol;
+    
+    private String descripcion;
+    
+    private Registro registro;
+
+    public Rol() {
+    }
+
+    public Rol(Integer idRol) {
+        this.idRol = idRol;
+    }
+
+    public Integer getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(Integer idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Registro getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(Registro registro) {
+        this.registro = registro;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idRol != null ? idRol.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Rol)) {
+            return false;
+        }
+        Rol other = (Rol) object;
+        if ((this.idRol == null && other.idRol != null) || (this.idRol != null && !this.idRol.equals(other.idRol))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.edu.ipn.cecyt9.edusite.model.Rol[ idRol=" + idRol + " ]";
+    }
+    
+}
---
+++ PreExamen/web/IndexJsp.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : IndexJsp
+    Created on : 8/10/2018, 08:16:39 AM
+    Author     : Alumno
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>PreExamen</title>
+    </head>
+    <body>
+        <h1>PreExamen Metztli Martínez</h1>
+    </body>
+</html>
